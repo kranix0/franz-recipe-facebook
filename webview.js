9a103603be4d62956183f8aae6579d1183e64136
Tony Ko <tony@htko.ca>
2017-11-23 11:10:13 -0500

    Add badge count retrieval via element inner html.

--- webview.js
+++ webview.js
@@ -1,7 +1,21 @@
 module.exports = (Franz) => {
   const getNotifications = function getNotifications() {
 
-    Franz.setBadge(1);
+		var count = 0;
+
+		var elements = [
+			document.getElementById('requestsCountValue'),
+			// document.getElementById('mercurymessagesCountValue'),
+			document.getElementById('notificationsCountValue')
+		]
+
+		elements.forEach(function(element){
+			if(element !== null && parseInt(element.innerHTML, 10)){
+				count += parseInt(element.innerHTML, 10);
+			}
+		})
+
+    Franz.setBadge(count);
   };
 
   // check for new messages every second and update Franz badge
